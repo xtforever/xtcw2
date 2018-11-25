321b280665a9afd2b79465c228aa1555b25445c5
Jens Harms <jharms@heyermedical.de>
2018-11-25 11:34:09 +0100

    init

--- build/include/xtcw/apputil.h
+++
@@ -1,6 +0,0 @@
-#ifndef APPUTIL_H
-#define APPUTIL_H
-
-#define STR_IS_EMPTY(a) (!((a) && *(a)))
-
-#endif
--- build/source/min-max.h
+++
@@ -1,7 +0,0 @@
-#ifndef MIN_MAX_H
-#define MIN_MAX_H
-
-#error "use mls.h instead"
-
-#endif
-
