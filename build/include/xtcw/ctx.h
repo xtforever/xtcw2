b49da3560d1e07bc6adadbb67005f1ae302a1311
jens harms <au1064@gmail.com>
2020-12-10 12:31:14 +0100

    cleanup1

--- build/include/xtcw/ctx.h
+++
@@ -1,9 +0,0 @@
-#ifndef CTX_H
-#define CTX_H
-
-
-int ctx_init(int *CTX,int length, int size );
-void ctx_free(int *CTX, int p, void (*free_cb)(int *ctx, int p));
-void ctx_destruct(int *CTX, void (*free_cb)(int *ctx, int p));
-
-#endif
