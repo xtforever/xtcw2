d694568cf5fc7e382576030a5884cee3123a27c0
jens harms <au1064@gmail.com>
2021-07-15 13:23:56 +0200

    add: missing test script

---
+++ bash-request/testshell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "hello, do u  want ice cream ( Y/n/a ) ?"
+echo >&2 this-is-an-error-msg
+read a
+if [ "$a" = "y" ]
+then
+    echo here is your ice cream
+    exit 0
+fi
+
+echo "why ? this is strange"
+exit 1
+
