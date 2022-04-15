a44cc658b812f7b063183190e010ccbc9b90b645
jens harms <au1064@gmail.com>
2022-04-15 10:59:11 +0200

    INIT dropdown

---
+++ dropdown/cwri.c
@@ -0,0 +1,272 @@
+/* most important define,
+   check APPNAME.ad !
+*/
+#define APP_NAME "cwri"
+
+/*
+ */
+
+#include "mls.h"
+
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>              /* Obtain O_* constant definitions */
+
+
+#include <X11/Intrinsic.h>
+#include <X11/StringDefs.h>
+#include <X11/Shell.h>
+#include <X11/Xmu/Editres.h>
+#include <X11/Vendor.h>
+#include <X11/Xaw/XawInit.h>
+
+#include <WcCreate.h>
+#include <Xp.h>
+#include <xutil.h>
+#include "wcreg2.h"
+#include <xtcw/register_wb.h>
+#include "njson_read.h"
+#include "var5.h"
+#include "xtcw/Wcombo.h"
+#include "xtcw/Wsqlcombo.h"
+#include "xtcw/WlistScroll.h"
+#include "xtcw/Wls.h"
+#include "xtcw/Wlist.h"
+#include "xtcw/Wcombo.h"
+#include "xtcw/Woptc.h"
+
+
+Widget TopLevel;
+int trace_main;
+#define TRACE_MAIN 2
+
+char *fallback_resources[] = {
+    APP_NAME ".allowShellResize: False",
+    "*WclResFiles:" APP_NAME ".ad\n",
+    "*traceLevel: 2",
+    NULL };
+
+/* All Wcl applications should provide at least these Wcl options:
+*/
+static XrmOptionDescRec options[] = {
+  { "-TraceLevel",	"*traceLevel",	XrmoptionSepArg, NULL },
+  WCL_XRM_OPTIONS
+};
+
+typedef struct CWRI_CONFIG {
+    int traceLevel;
+} CWRI_CONFIG;
+
+#define FLD(n)  XtOffsetOf(CWRI_CONFIG,n)
+static XtResource CWRI_CONFIG_RES [] = {
+
+  { "traceLevel", "TraceLevel", XtRInt, sizeof(int),
+   FLD(traceLevel), XtRImmediate, 0
+  }
+
+};
+#undef FLD
+
+struct CWRI_CONFIG CWRI;
+
+void test_cb( Widget w, void *u, void *c )
+{
+  printf("cb\n");
+
+  if(! XtIsSubclass(w, weditWidgetClass ) ) return;
+  char *s;
+  XtVaGetValues(w, "label", &s, NULL );
+  if( is_empty(s) ) return;
+  printf("Label: %s\n", s );
+
+}
+
+void quit_cb( Widget w, void *u, void *c )
+{
+    TRACE(1,"QUIT");
+    XtAppSetExitFlag( XtWidgetToApplicationContext(w) );
+}
+
+
+
+/* --------------------------------------------------------------------------------------------------------------------
+
+                        IMPLEMENTATION
+
+
+                        Must Provide
+                        - RegisterApplication
+                        - InitializeApplication
+
+  -------------------------------------------------------------------------------------------------------------------- */
+
+static void RegisterApplication ( Widget top )
+{
+    /* -- Register widget classes and constructors */
+    // RCP( top, wbatt );
+    /* -- Register application specific actions */
+    /* -- Register application specific callbacks */
+    RCB( top, quit_cb );
+    RCB( top, test_cb );
+    RCP( top, wls );
+    RCP( top, woptc );
+    RCP( top, wlist );
+    RCP( top, wlistScroll );
+    RCP( top, wcombo );
+    RCP( top, wsqlcombo );
+}
+
+/*  init application functions and structures and widgets
+    All widgets are created, but not visible.
+    functions can now communicate with widgets
+*/
+static void InitializeApplication( Widget top )
+{
+    trace_level = CWRI.traceLevel;
+}
+
+/******************************************************************************
+**  Private Functions
+******************************************************************************/
+
+static void syntax(void)
+{
+  puts( syntax_wcl );
+  puts( "-TraceLevel <num>\n"
+	"-ListenPort <num>\n" );
+}
+
+
+
+int njson_cmp(char *name, struct njson_st *j)
+{
+  if( j->name ) return mstrcmp(j->name,0,name);
+  return 1;
+}
+
+
+struct njson_st *njson_find_data(int opts, char *name)
+{
+  int p;
+  struct njson_st *j;
+  m_foreach(opts,p,j) {
+    if( !njson_cmp(name,j) ) return j;
+    if( j->typ >= NJSON_ARR ) {
+      j=njson_find_data(j->d,name);
+      if( j ) return j;
+    }
+  }
+  return NULL;
+}
+
+
+char *njson_value( struct njson_st *j, int p )
+{
+  if( (!j) || (!j->d)) { return ""; }
+  if(j->typ  >= NJSON_ARR) {
+    if( m_len(j->d) > p ) return njson_value( mls(j->d,p), 0 );
+    return "";
+  }  
+  return CHARP(j->d);
+}
+
+char *njson_find(int opts, char *name, int p)
+{
+  struct njson_st *j = njson_find_data(opts, name);  
+  return njson_value(j,p);
+}
+
+
+
+/******************************************************************************
+*   MAIN function
+******************************************************************************/
+int main ( int argc, char **argv )
+{
+    trace_main = TRACE_MAIN;
+
+    XtAppContext app;
+    m_init();
+    mvar_init();
+
+    char *xs = "( test: 5, sample: (1,2,3,4,5,6, gum: \"red\" ) )";
+    int dt = njson_from_string( xs );
+    char *sample = njson_find( dt, "gum", 0 );
+    printf("sample=%s\n", sample );
+    
+    
+
+    
+    XtSetLanguageProc (NULL, NULL, NULL);
+    XawInitializeWidgetSet();
+
+    /*  --  Intialize Toolkit creating the application shell
+     */
+    Widget appShell = XtOpenApplication (&app, APP_NAME,
+             /* resources: can be set from argv */
+             options, XtNumber(options),
+	     &argc, argv,
+	     fallback_resources,
+	     sessionShellWidgetClass,
+	     NULL, 0
+	   );
+
+
+    /*  --  Enable Editres support
+     */
+    XtAddEventHandler(appShell, (EventMask) 0, True, _XEditResCheckMessages, NULL);
+
+    XtAddCallback( appShell, XtNdieCallback, quit_cb, NULL );
+
+    /*  --  not parsed options are removed by XtOpenApplication
+            the only entry left should be the program name
+    */
+    if (argc != 1) { m_destruct(); syntax(); exit(1); }
+    TopLevel = appShell;
+
+
+    /*  --  Register all application specific
+            callbacks and widget classes
+    */
+    RegisterApplication ( appShell );
+
+    /*  --  Register all Athena and Public
+            widget classes, CBs, ACTs
+    */
+    XpRegisterAll ( app );
+
+    /*  --  Create widget tree below toplevel shell
+            using Xrm database
+    */
+    WcWidgetCreation ( appShell );
+
+
+    /*  -- Get application resources and widget ptrs
+     */
+    XtGetApplicationResources(	appShell, (XtPointer)&CWRI,
+				CWRI_CONFIG_RES,
+				XtNumber(CWRI_CONFIG_RES),
+				(ArgList)0, 0 );
+
+    InitializeApplication(appShell);
+
+    /*  --  Realize the widget tree and enter
+            the main application loop  */
+    XtRealizeWidget ( appShell );
+
+    grab_window_quit( appShell );
+
+    XtAppMainLoop ( app ); /* use XtAppSetExitFlag */
+    // XtDestroyWidget(appShell);
+
+    mvar_destruct();
+    m_destruct();
+
+    return EXIT_SUCCESS;
+}
---
+++ dropdown/mls.c
@@ -0,0 +1,1 @@
+../utils/mls.c
